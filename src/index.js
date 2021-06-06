6c52c57499c306bb58ca256226206c3da3ee335a
rohan kumar <rohan_1901ee48@iitp.ac.in>
2021-06-06 18:52:20 +0530

    resolveerror

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app=express();
 
 const server=http.createServer(app);//ccreating server outside of express
 const io=socketio(server);// our server support websocket
-const port=process.env.port||3000
+const port=process.env.PORT||3000
 // serve up public directory
 const publicDirectoryPath=path.join(__dirname,'../public')
 
